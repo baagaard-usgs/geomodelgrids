adf18f9952313a54e23135dcf096fa1b2a510a04
Brad Aagaard <baagaard@usgs.gov>
2019-09-23 08:51:20 -0700

    Read facedump.

--- bin/read_facedump.py
+++ bin/read_facedump.py
@@ -2,19 +2,27 @@ import re
 
 FILENAME = "facedump.txt"
 
+def read_names(lines, start):
+    id_mapping = {}
+    pattern = r"^\[([0-9]+)\]\s([\S]+.+)"
+    for line in lines[start:]:
+        match = re.search(pattern, line.strip())
+        if match and len(match.groups()) == 2:
+            groups = match.groups()
+            id_mapping[groups[1]] = groups[0]
+        else:
+            break
+    return id_mapping
+
+
 with open(FILENAME, "r") as fin:
     lines = fin.readlines()
 
 for iline, line in enumerate(lines):
     if line.startswith("Fault Block Names"):
-        read_fault_blocks(lines, iline+1)
+        fault_blocks = read_names(lines, iline+1)
     if line.startswith("Zone Names"):
-        read_zones(lines, iline+1)
+        zones = read_names(lines, iline+1)
 
-def read_fault_blocks(lines, start):
-    format = 
-    for line in lines[start:]:
-        
-        if not line.strip():
-            break
-        
+print(fault_blocks)
+print(zones)
