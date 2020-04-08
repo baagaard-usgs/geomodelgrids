a38804e2f1f514bcc053d25149364139c2db6caa
Brad Aagaard <baagaard@usgs.gov>
2020-04-08 13:54:48 -0600

    Add header files for constants and high-level API.

---
+++ libsrc/geomodelgrids/geomodelgrids_serial.hh
@@ -0,0 +1,12 @@
+/** High-level header file for geomodelgrids library.
+ * This file can be used as the include for the query api.
+ */
+
+#if !defined(geomodelgrids_serial_hh)
+#define geomodelgrids_serial_hh
+
+#include "serial/Query.hh"
+
+#endif // geomodelgrids_serial_hh
+
+// End of file
