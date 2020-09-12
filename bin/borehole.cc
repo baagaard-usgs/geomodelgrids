d864e80eea303be23d4b655cfa1f4d1ce0cf5ef1
Brad Aagaard <baagaard@usgs.gov>
2020-09-11 20:47:38 -0600

    Catch exceptions in app drivers.

--- bin/borehole.cc
+++ bin/borehole.cc
@@ -2,11 +2,26 @@
 
 #include "geomodelgrids/apps/Borehole.hh" // USES Borehole
 
+#include <stdexcept> // USES std::exception
+#include <iostream> // USES std::cerr
+
 int
 main(int argc,
      char* argv[]) {
     geomodelgrids::apps::Borehole borehole;
-    return borehole.run(argc, argv);
+
+    int err = 0;
+    try {
+      err = borehole.run(argc, argv);
+    } catch (const std::exception& ex) {
+	std::cerr << ex.what() << std::endl;
+	err = 1;
+    } catch (...) {
+      std::cerr << "Caught unknown exception." << std::endl;
+      err = 2;
+    } // try/catch
+
+    return err;
 } // main
 
 
