d6d0d182db97981ba9cdb0ba9ad998f688153297
Brad Aagaard <baagaard@usgs.gov>
2023-07-26 10:38:28 -0600

    Clean up builder Python script for Docker images.

--- docker/builder.py
+++ docker/builder.py
@@ -6,38 +6,19 @@
 import os
 import argparse
 import subprocess
+import pathlib
 
 class DockerApp(object):
     """Application for building and pushing docker images.
     """
+    def __init__(self, docker_file, registry="code.usgs.gov:5001/ghsc/gmp/earth-structure/cvms/geomodelgrids", prefix="testenv"):
+        self.docker_filename = docker_file
+        self.registry = registry
 
-    def __init__(self):
-        self.docker_filename = None
-        self.tag = None
+        self.tag = self._get_tag(docker_file, registry, prefix)
 
-    def main(self, **kwargs):
-        """Main entry point
-        """
-        args = argparse.Namespace(**kwargs) if kwargs else self._parse_command_line()
-        self.initialize(args)
-
-        if args.build:
-            self.build(args.build_env)
-
-        if args.push:
-            self.push()
-
-    def initialize(self, args):
-        """Initialize builder.
-        """
-        self.docker_filename = args.dockerfile
-        if args.prefix[-1] == "/":
-            self.tag = args.prefix + os.path.split(args.dockerfile)[-1]
-        else:
-            self.tag = "-".join([args.prefix, os.path.split(args.dockerfile)[-1]])
-
-    def build(self, build_env):
-        """Build docker image.
+    def build(self, build_env="certs-doi"):
+        """Build image.
         """
         if build_env:
             cmd = f"docker build -t {self.tag} -f {self.docker_filename} --build-arg BUILD_ENV={build_env} ."
@@ -46,22 +27,20 @@ class DockerApp(object):
         self._run_cmd(cmd)
 
     def push(self):
-        """Push docker image to container registry.
+        """Push docker image to registry.
         """
-        cmd = "docker push {tag}".format(tag=self.tag)
+        cmd = f"docker push {self.tag}"
         self._run_cmd(cmd)
 
-    @staticmethod
-    def _parse_command_line():
-        """Parse command line arguments.
+    def _get_tag(self, docker_file, registry, prefix):
+        """Get image tag.
         """
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--dockerfile", action="store", dest="dockerfile", required=True)
-        parser.add_argument("--prefix", action="store", dest="prefix", default="registry.gitlab.com/baagaard-usgs/geomodelgrids/testenv")
-        parser.add_argument("--build", action="store_true", dest="build")
-        parser.add_argument("--push", action="store_true", dest="push")
-        parser.add_argument("--build-env", action="store", dest="build_env", default=None)
-        return parser.parse_args()
+        image = pathlib.Path(docker_file).name
+        if prefix:
+            tag = f"{registry}/{prefix}-{image}"
+        else:
+            tag = f"{registry}/{image}"
+        return tag
 
     @staticmethod
     def _run_cmd(cmd):
@@ -69,11 +48,32 @@ class DockerApp(object):
         """
         print("Running '%s'..." % cmd)
         subprocess.check_call(cmd.split())
+        return
 
 
-# ======================================================================
+def cli():
+    """Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dockerfile", action="store", dest="docker_file", required=True)
+    parser.add_argument("--registry", action="store", dest="registry", default="code.usgs.gov:5001/ghsc/gmp/earth-structure/cvms/geomodelgrids")
+    parser.add_argument("--prefix", action="store", dest="prefix", default="testenv")
+    parser.add_argument("--build", action="store_true", dest="build")
+    parser.add_argument("--push", action="store_true", dest="push")
+    parser.add_argument("--build-env", action="store", dest="build_env", default=None, choices=(None,"nocerts","certs-doi"))
+
+    args = parser.parse_args()
+
+    app = DockerApp(args.docker_file, args.registry, args.prefix)
+    if args.build:
+        app.build(args.build_env)
+    if args.push:
+        app.push()
+
+
+# =================================================================================================
 if __name__ == "__main__":
-    DockerApp().main()
+    cli()
 
 
 # End of file
